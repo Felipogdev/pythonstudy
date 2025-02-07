325417de0ffd8a8e35d963cc95a54142115d9538
Felipogdev <felipedantaslaurentino@gmail.com>
2025-02-07 20:25:30 +0000

    .

--- ml_intro/iowa_model.py
+++ ml_intro/iowa_model.py
@@ -1,6 +1,9 @@
+from sklearn.metrics import mean_absolute_error
 import pandas as pd
 import kagglehub
-
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from get_mae import get_mae 
 
 path = kagglehub.dataset_download("marcopale/housing")
 
@@ -12,10 +15,6 @@ features = ['Total Bsmt SF', 'Gr Liv Area', 'Garage Area', '1st Flr SF', '2nd Fl
 X = iowa_df[features]
 y = iowa_df.SalePrice
 
-
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-#Splitting my data to train
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 1)
 
 iowa_model = DecisionTreeRegressor(random_state = 1)
@@ -24,7 +23,22 @@ iowa_model.fit(train_X, train_y)
 
 val_predicitons = iowa_model.predict(val_X)
 
-from sklearn.metrics import mean_absolute_error
-
 val_mae = mean_absolute_error(val_y, val_predicitons)
 print(val_mae)
+
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+best_mae = float("inf")
+best_tree_size=None
+
+for max_leaf_nodes in candidate_max_leaf_nodes:
+    temp_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    if temp_mae < best_mae:
+        best_mae = temp_mae
+        best_tree_size = max_leaf_nodes
+
+final_model = DecisionTreeRegressor(max_leaf_nodes = best_tree_size, random_state = 1)
+final_model.fit(X, y)
+final_model_predictions = final_model.predict(val_X)
+final_model_val_mae = mean_absolute_error(val_y, final_model_predictions)
+print(final_model_val_mae)
+
